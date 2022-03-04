0a1d64530bf332435be53b70610f5a1cb84382b9
rzhasibuan <rzhasibuan@gmail.com>
2022-03-04 13:33:55 +0700

    setup project

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        $user = User::all()->count();
+        
+
+        return view('home', [
+            'title' => "Dashboard",
+            'subDashboard' => 'active',
+            'user' => $user,
+        ]);
+    }
+}
