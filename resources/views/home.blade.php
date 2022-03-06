0c2113855a9f8398f98a583d4e57b3de210e652c
rzhasibuan <rzhasibuan@gmail.com>
2022-03-06 13:53:58 +0700

    repair admin dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,10 +10,10 @@
                     <div class="inner">
                         {{-- <h3>{{$pasien}}</h3> --}}
 
-                        <p>Patient / Pasien</p>
+                        <p>Surat Masuk</p>
                     </div>
                     <div class="icon">
-                        <i class="ion ion-person"></i>
+                        <ion-icon name="mail-unread-outline"></ion-icon>
                     </div>
                     <a href="{{route('admin.pasien.index')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                 </div>
@@ -25,10 +25,10 @@
                     <div class="inner">
                         {{-- <h3>{{$gejala}}</h3> --}}
 
-                        <p>Symptom / Gejala</p>
+                        <p>Surat Keluar</p>
                     </div>
                     <div class="icon">
-                        <i class="ion ion-medkit"></i>
+                        <ion-icon name="mail-outline"></ion-icon>
                     </div>
                     <a href="{{route('admin.gejala.index')}}" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
                 </div>
@@ -49,63 +49,8 @@
                 </div>
             </div>
         </div>
-    <h3>Berikut hasil permeriksaaan</h3>
-    <div class="box box-solid bg-teal-gradient">
-        <div class="box-footer no-border">
-            <div class="row">
-                <div class="col-xs-4 text-center" style="border-right: 1px solid #f4f4f4">
-                    {{-- <input type="text" class="knob" data-readonly="true" value="{{$statusNegatif}}" data-width="60" data-height="60" --}}
-                           data-fgColor="#39CCCC">
 
-                    <div class="knob-label">Negatif</div>
-                </div>
-                <!-- ./col -->
-                <div class="col-xs-4 text-center" style="border-right: 1px solid #f4f4f4">
-                    {{-- <input type="text" class="knob" data-readonly="true" value="{{$statusKemungkinanBesar}}}" data-width="60" data-height="60"
-                           data-fgColor="#39CCCC"> --}}
-
-                    <div class="knob-label">Kemungkinan Besar</div>
-                </div>
-                <!-- ./col -->
-                <div class="col-xs-4 text-center">
-                    {{-- <input type="text" class="knob" data-readonly="true" value="{{$statusPositif}}" data-width="60" data-height="60"
-                           data-fgColor="#39CCCC"> --}}
-
-                    <div class="knob-label">Positif</div>
-                </div>
-                <!-- ./col -->
-            </div>
-            <!-- /.row -->
-        </div>
-    </div>
     @endrole
-    {{-- @role('pasien')
-    <div class="box box-solid bg-teal-gradient">
-        <div class="box-body">
-            <h5 class="text-center">Berikut hasil diagnosa anda</h5>
-        </div>
-        <div class="box-footer no-border">
-            <div class="row">
-                 <div class="col-xs-4 col-lg-12 text-center">
-                     @if($statusPasien->value == null)
-                         <input type="text" class="knob" data-readonly="true" value="0" data-width="100" data-height="100"
-                                data-fgColor="red">
-                     @else
-                         <input type="text" class="knob" data-readonly="true" value="{{$statusPasien->value}}" data-width="100" data-height="100"
-                                data-fgColor="red">
-                     @endif
-                     @if($statusPasien->status == null)
-                         <div class="knob-label">Belum di diagnosa</div>
-                     @else
-                         <div class="knob-label">{{$statusPasien->status}}</div>
-                     @endif
-                </div>
-                <!-- ./col -->
-            </div>
-            <!-- /.row -->
-        </div>
-    </div>
-    @endrole --}}
 </div>
 @endsection
 
