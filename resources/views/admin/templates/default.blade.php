b920fa2be7375f16545348e59bb8b3eddbfa482a
rzhasibuan <rzhasibuan@gmail.com>
2022-03-11 23:56:49 +0700

    admin dashboard

--- resources/views/admin/templates/default.blade.php
+++ resources/views/admin/templates/default.blade.php
@@ -22,7 +22,7 @@ desired effect
 |               | sidebar-mini                               |
 |---------------------------------------------------------|
 -->
-<body class="hold-transition skin-purple sidebar-mini">
+<body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
 
   <!-- Main Header -->
